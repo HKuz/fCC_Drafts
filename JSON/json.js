0f5550dd726baca71d28a0cc9d56c4f17217f086
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-03-22 16:07:18 -0400

    Create JSON folder for new curriculum project

---
+++ JSON/json.js
@@ -0,0 +1,15 @@
+{
+    "Team": "Argentina",
+    "Sport": "Football",
+    "Year": 1986,
+    "World Cup Winner": true,
+    "Head Coach": "Carlos Bilardo",
+    "Players": [
+        {
+            "Name": "Diego Maradona",
+            "Position": "MF",
+            "Number": 10,
+            "isCaptain": true
+        }
+    ]
+}
