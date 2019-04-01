d4bbcfb17be091e87085ebcafa1aaccdc89b187a
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-04-01 07:09:49 -0400

    Create JSON challenge 01

---
+++ JSON/Challenges/index01.js
@@ -0,0 +1,13 @@
+
+
+/*
+JavaScript Object Notation (JSON) is a format to structure data.
+Because JSON is lightweight, human readable, and easily parsed by machines, it's a popular structure that's used to exchange data over the web.
+It started as a JavaScript standard, but now it is language independent and can be used in many programming languages.
+
+JSON is built using two structures:
+1. Objects: unordered collections of name-value pairs, surrounded by `{ }`
+2. Arrays: ordered collections of values, surrounded by `[ ]`
+
+Declare a new variable using the `let` keyword and name it `myJSON`, then set it equal to an empty object.
+*/
