d4575d16d7768001ffd9f9ce61649918efd1f9ff
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-04-01 09:51:27 -0400

    Add valid-not valid note to JSON challenge 10

--- JSON/Challenges/index10.js
+++ JSON/Challenges/index10.js
@@ -19,5 +19,11 @@ let myTeam = {
 }
 
 /*
-Add another object in the `Players` array after Sergio's. Include a `Name` key with a value of `Diego Maradona` and a `Position` key with a value of `MF`.
+Now you've seen an example of each valid data type in JSON: numbers, strings, Boolean values, objects, arrays, and null.
+
+JSON can look a lot like JavaScript object literal sytax, but there are two differences worth noting here:
+1. JSON is a data format only - it's used to hold properties, so including methods in an object is NOT valid JSON
+2. JSON doesn't even need to be an object (`{ }`) - any of the data types mentioned above could be valid JSON on their own (a single string, an array, etc.)
+
+Let's add one more player into the `Players` array after Sergio's object for practice. Include a `Name` key with a value of `Diego Maradona` and a `Position` key with a value of `MF`.
 */
