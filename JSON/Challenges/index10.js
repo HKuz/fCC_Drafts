3f3c995c29076a934f1ccaff6cfee3bd0be7ed07
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-04-01 09:19:40 -0400

    Create JSON challenge 10

---
+++ JSON/Challenges/index10.js
@@ -0,0 +1,23 @@
+let myTeam = {
+    "Team": "Argentina",
+    "Sport": "Football",
+    "Year": 1986,
+    "World Cup Winner": true,
+    "Head Coach": {
+        "Name": "Carlos Bilardo",
+        "Matches": 7
+    },
+    "Players": [
+        {
+            "Name": "Sergio Batista",
+            "Position": "MF",
+            "Number": 2,
+            "isCaptain": false,
+            "Nickname": null
+        }
+    ]
+}
+
+/*
+Add another object in the `Players` array after Sergio's. Include a `Name` key with a value of `Diego Maradona` and a `Position` key with a value of `MF`.
+*/
