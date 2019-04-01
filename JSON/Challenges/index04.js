b4d0c025623eaf47f2b9623a8226968589d49290
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-04-01 08:26:32 -0400

    Create JSON challenge 04

---
+++ JSON/Challenges/index04.js
@@ -0,0 +1,9 @@
+let myTeam = {
+    "Team": "Argentina",
+    "Sport": "Football",
+    "Year": 1986
+}
+
+/*
+Add another key-value pair after `Year`, this time for `World Cup Winner`, this time using a Boolean value set to `true`.
+*/
