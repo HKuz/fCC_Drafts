7993cd7407c5aa73df9b57ee143012dfdff70cd3
Heather Kusmierz <heather.kusmierz@gmail.com>
2019-04-01 07:43:32 -0400

    Create JSON challenge 02

---
+++ JSON/Challenges/index02.js
@@ -0,0 +1,18 @@
+let myTeam = {}
+
+/*
+The last challenge mentioned that you structure your data in JSON with objects and arrays.
+Both of those structures contain values, which can be:
+- Numbers
+- Boolean values (`true` or `false`)
+- Strings (must use double quotes)
+- Objects
+- Arrays
+- null
+
+Note that the JSON specification requires you to surround any string with double quotes (`" "`), not single quotes (`' '`)! This applies to strings used as keys or values.
+
+We're going to build a structure in JSON to hold the 1986 World Cup champion team from Argentina.
+
+Start by adding a new key into your `myTeam` object called `Team` with a string value of `Argentina`.
+*/
